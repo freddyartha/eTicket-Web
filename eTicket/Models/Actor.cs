0b3cf69b38cb7e95c1ec411ec14264433f041f77
Freddy Artha <freddyartha@gmail.com>
2023-05-26 16:45:06 +0800

    first model

---
+++ eTicket/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicket.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public String ProfilePictureURl { get; set; }
+        public String FullName { get; set; }
+        public String Bio { get; set; }
+    }
+}
---
+++ eTicket/Models/Producer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicket.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+        public String ProfilePictureURl { get; set; }
+        public String FullName { get; set; }
+        public String Bio { get; set; }
+    }
+}
